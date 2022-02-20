8f00f0b613992085b3cf685cc219b7c6c74b659d
XSkolzkiyX <m.polyakov2013@gmail.com>
2022-02-20 20:06:46 +0200

    Modified AI

--- Assets/Scripts/StudentAI.cs
+++ Assets/Scripts/StudentAI.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class StudentAI : MonoBehaviour
 {
+    public bool streetStudent;
     public float speed = 10f;
+    public GameObject sprite;
     public Transform[] Targets = new Transform[10];
     Transform curTarget = null;
 
+
     void Update()
     {
-        if(curTarget == null || transform.position == curTarget.position)
-            curTarget = Targets[Random.Range(0, Targets.Length)];
-        transform.position = Vector3.MoveTowards(transform.position, new Vector2(curTarget.position.x, transform.position.y), speed * Time.deltaTime);
+        if (streetStudent)
+        {
+            if (curTarget == null || transform.position == curTarget.position)
+                SetTarget();
+        }
+        else
+        {
+            if (curTarget == null)
+                SetTarget();
+            if (transform.position == curTarget.position) Destroy(gameObject);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, curTarget.transform.position, speed * Time.deltaTime);
+    }
+
+    void SetTarget()
+    {
+        curTarget = Targets[Random.Range(0, Targets.Length)];
+        if (curTarget.transform.position.x >= transform.position.x)
+            sprite.transform.Rotate(0, 0, 0);
+        else
+            sprite.transform.Rotate(0, 180, 0);
     }
 }
